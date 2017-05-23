ea74db462b75c2ddd8b9a9e3b426918a1e3090cd
Alexandre Dufournet <alex.dufournet@theguardian.com>
2017-05-23 14:42:41 +0100

    add more logs for http 400s

--- registration/app/registration/LogHttpErrorHandler.scala
+++ registration/app/registration/LogHttpErrorHandler.scala
@@ -1,10 +1,11 @@
 package registration
 
 import play.api.{Configuration, Environment, Logger}
-import play.api.http.{DefaultHttpErrorHandler}
+import play.api.http.DefaultHttpErrorHandler
 import play.api.mvc.{RequestHeader, Result}
 import play.api.routing.Router
 import play.core.SourceMapper
+import play.mvc.Http
 
 import scala.concurrent.Future
 
@@ -14,7 +15,7 @@ class LogHttpErrorHandler(
   sourceMapper: Option[SourceMapper] = None,
   router: => Option[Router] = None) extends DefaultHttpErrorHandler(environment, configuration, sourceMapper, router) {
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
-    Logger.error(s"HTTP $statusCode $message $request")
+    Logger.error(s"HTTP $statusCode $request $message ${request.headers.get(Http.HeaderNames.USER_AGENT)}")
     super.onClientError(request, statusCode, message)
   }
 
